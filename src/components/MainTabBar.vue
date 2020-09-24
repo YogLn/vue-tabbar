7428030f4f0255e2d4da1d66572fe98877339008
YogLn <15508067205@163.com>
2020-09-24 16:15:15 +0800

    tabber

---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="blue">
+        <img slot="item-icon" src="~assets/image/tabbar/home.svg" >
+        <img slot="item-icon-active" src="~assets/image/tabbar/home_active.svg" >
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="blue">
+        <img slot="item-icon" src="~assets/image/tabbar/category.svg" >
+        <img slot="item-icon-active" src="~assets/image/tabbar/category_active.svg" >
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/shopcar" activeColor="blue" >
+        <img slot="item-icon" src="~assets/image/tabbar/shopcar.svg" >
+        <img slot="item-icon-active" src="~assets/image/tabbar/shopcar_active.svg" >
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="~assets/image/tabbar/profile.svg" >
+        <img slot="item-icon-active" src="~assets/image/tabbar/profile_active.svg" >
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "components/tabbar/TabBar"
+import TabBarItem from "components/tabbar/TabBarItem"
+export default {
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+const Home = () => import("../views/home/Home.vue");
+const Category = () => import("../views/category/Categor.vue");
+const Shopcar = () => import("../views/shopcar/Shopcar.vue");
+const Profile = () => import("../views/profile/Profile.vue");
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      redirect: "/home"
+    },
+    {
+      path: "/home",
+      component: Home
+    },
+    {
+      path: "/category",
+      component: Category
+    },
+    {
+      path: "/shopcar",
+      component: Shopcar
+    },
+    {
+      path: "/profile",
+      component: Profile
+    }
+  ],
+  mode: "history"
+});
